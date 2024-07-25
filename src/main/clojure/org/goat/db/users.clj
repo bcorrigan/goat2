4143f1527f1fcc952fc8e9a5a25d924430e29b8a
bcorrigan <b.j.corrigan@gmail.com>
2024-07-25 23:36:23 +0100

    Have a record keeping facility

--- src/main/clojure/org/goat/db/users.clj
+++ src/main/clojure/org/goat/db/users.clj
@@ -1,4 +1,5 @@
-(ns org.goat.db.users (:require [clojure.java.jdbc :refer :all]))
+(ns org.goat.db.users (:require [clojure.java.jdbc :refer :all]
+                                [clojure.edn :as edn]))
 
 (def db
   {:classname "org.sqlite.JDBC"
@@ -27,8 +28,60 @@
                                           [:starttime :datetime]
                                           [:endtime :datetime]
                                           ]))
+       (db-do-commands db
+                       (create-table-ddl :records
+                                         [[:username :text]
+                                          [:record :text]
+                                          [:recordval :text]
+                                          [:recordtime :datetime]]))
+
+       (execute! db "create unique index recidx on records(username, record)" )
        (catch Exception e
          (println "Fatal error" (.getMessage e)))))
+
+(defn get-losttime
+  "Return timestamp at which given user last lost a normal game"
+  [user]
+  (get (first (query db [(format (str "select max(endtime) as losttime"
+                                      " from wordlegames"
+                                      " where won=false"
+                                      " and type='single'"
+                                      " and size=5"
+                                      " and difficulty='easy'"
+                                      " and username='%s'") user)])) :losttime))
+
+(defn get-streak
+  "Return the current streak for the given user."
+  [user]
+  (let [losttime (get-losttime user) ]
+    (get (first (query db [(format (str "select count(*) as streak "
+                                   " from wordlegames"
+                                   " where won=true"
+                                   " and type='single'"
+                                   " and size=5"
+                                   " and difficulty='easy' "
+                                   " and endtime>%d"
+                                   " and username='%s'") losttime user)])) :streak)))
+
+
+;; not sure how this might behave if we have non-integer record types
+(defn get-record
+  "Get some record from records table"
+  [user record]
+  (edn/read-string (get (first (query db [(format (str "select recordval as record"
+                          " from records"
+                          " where username='%s'"
+                          " and record='%s'") user (symbol record))])) :record)))
+
+;; needs to be an upsert really
+(defn save-record
+  "Update the given record in records table"
+  [user record recordval time]
+  (execute! db [(str "insert into records (username, record, recordval, recordtime) "
+                     " values(?,?,?,?)"
+                     " on conflict(username, record)"
+                     " do update set recordval=excluded.recordval,"
+                     " recordtime=excluded.recordtime") user record recordval time  ]))
 
 (defn record-wordle-game
   "Save all interesting facts about a game of wordle."
@@ -50,9 +103,13 @@
                             :g6 (get (get match :guesses) 5)
                             :starttime (get match :starttime)
                             :endtime (get match :endtime)
-                            }))
-
-(print "hey")
+                            })
+  (let [user (get match :user)
+        streak (get-streak user)
+        max-streak (or (get-record user :streak) 0)]
+    (if (> streak max-streak)
+      (save-record user :streak streak (get match :endtime))
+    )))
 
 (defn best-wordle-player
   "Who is the best at standard wordle?"
@@ -62,28 +119,3 @@
                                               where type='single'
                                               and size=5
                                               and difficulty='easy'"))]))
-
-(defn get-losttime
-  "Return timestamp at which given user last lost a normal game"
-  [user]
-  (get (first (query db [(format (str "select max(endtime) as losttime"
-                                      " from wordlegames"
-                                      " where won=false"
-                                      " and type='single'"
-                                      " and size=5"
-                                      " and difficulty='easy'"
-                                      " and username='%s'") user)])) :losttime))
-
-
-(defn get-streak
-  "Return the current streak for the given user."
-  [user]
-  (let [losttime (get-losttime user) ]
-    (get (first (query db [(format (str "select count(*) as streak "
-                                   " from wordlegames"
-                                   " where won=true"
-                                   " and type='single'"
-                                   " and size=5"
-                                   " and difficulty='easy' "
-                                   " and endtime>%d"
-                                   " and username='%s'") losttime user)])) :streak)))
